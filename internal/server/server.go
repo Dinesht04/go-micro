524576628a5e1956f9b4ea8ae70bd515feee47f7
Dinesht04 <dineshtyagi567@gmail.com>
2026-01-12 21:26:23 +0530

    server init

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,5 +1,11 @@
 package server
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
 type Server struct {
 	Port string
 }
@@ -13,4 +19,14 @@ func NewServer() *Server {
 
 func StartServer() {
 	//start server and pass params into redis
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	//8080
+	r.Run()
 }
